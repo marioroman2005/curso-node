c6f3a512038af52b9154aa2dd11341b5d9b08f82
marioroman2005 <mariorota2005@gmail.com>
2026-02-04 20:53:34 +0100

    1h: 15 min

---
+++ cjs/sum.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+  return a + b;
+}
+//CommonJS module export
+module.exports = { sum };
---
+++ mjs/index.mjs
@@ -0,0 +1,9 @@
+// .js -> Por defecto utiliza commonjs
+// .mjs -> Para utilizar ES modules
+// .cjs -> Para forzar que se utilize commonjs
+
+import { sum, sub, mult } from "./sum.mjs";
+
+console.log(sum(1, 2));
+console.log(sub(1, 2));
+console.log(mult(1, 2));
