f02c66d4ce1a3a224fff861275ea3d0ff50c5193
marioroman2005 <mariorota2005@gmail.com>
2026-02-06 11:25:11 +0100

    min 52

--- Clase_2/2.routing.js
+++ Clase_2/2.routing.js
@@ -1,6 +1,35 @@
 const http = require('node:http')
 
-const processRequest = (req, res) => {}
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json ; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html ; charset=utf-8')
+          return res.end('<h1>404</h1>')
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon':
+          const body = ''
+          break
+          
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain ; charset=utf-8')
+          return res.end('404 not found')
+      }
+  }
+}
 
 const server = http.createServer(processRequest)
 
