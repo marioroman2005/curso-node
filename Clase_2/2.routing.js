6bf890b6e251cf2f55730e957e35d53a90ca35dc
marioroman2005 <mariorota2005@gmail.com>
2026-02-08 16:14:29 +0100

    Clase 2

--- Clase_2/2.routing.js
+++ Clase_2/2.routing.js
@@ -1,5 +1,6 @@
 const http = require('node:http')
 
+// commonJS -> modulos clásicos de node
 const dittoJSON = require('./pokemon/ditto.json')
 
 const processRequest = (req, res) => {
@@ -9,24 +10,41 @@ const processRequest = (req, res) => {
     case 'GET':
       switch (url) {
         case '/pokemon/ditto':
-          res.setHeader('Content-Type', 'application/json ; charset=utf-8')
+
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
           return res.end(JSON.stringify(dittoJSON))
         default:
           res.statusCode = 404
-          res.setHeader('Content-Type', 'text/html ; charset=utf-8')
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
           return res.end('<h1>404</h1>')
       }
 
     case 'POST':
       switch (url) {
-        case '/pokemon':
-          const body = ''
+        case '/pokemon': {
+          let body = ''
+
+          // escuchar el evento data
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
+
           break
-          
+        }
+
         default:
           res.statusCode = 404
-          res.setHeader('Content-Type', 'text/plain ; charset=utf-8')
-          return res.end('404 not found')
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          return res.end('404 Not Found')
       }
   }
 }
@@ -34,5 +52,5 @@ const processRequest = (req, res) => {
 const server = http.createServer(processRequest)
 
 server.listen(1234, () => {
-  console.log('Server listening on port http://localhost:1234')
+  console.log('server listening on port http://localhost:1234')
 })
