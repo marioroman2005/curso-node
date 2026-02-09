3d764490ce2b742ff8610976eb4759c5c2142b65
marioroman2005 <mariorota2005@gmail.com>
2026-02-09 14:42:08 +0100

    Min 32 tercer curso

--- Clase_3/app.js
+++ Clase_3/app.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const movies = require('./movies.json')
 
 const app = express()
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hola mundo' })
-})
 const PORT = process.env.PORT ?? 1234
+
+// TODOS LOS RECURSOS QUE SEAN MOVIES SE IDENTIFICAN CON /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+
+    return res.json(filteredMovies)
+  }
+
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path to regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
